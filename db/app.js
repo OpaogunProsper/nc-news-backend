6ff47d874b31359360e48c62c6c260729e186ff0
OpaogunProsper <prblvck@gmail.com>
2024-07-16 16:22:03 +0100

    gets the articles

--- db/app.js
+++ db/app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const { getApi } = require("./controllers/api.controllers");
 const { getTopics } = require("./controllers/topics.controllers");
-const { getArticleById } = require("../db/controllers/articles.controllers");
+const {
+  getArticleById,
+  getAllArticles,
+} = require("../db/controllers/articles.controllers");
 const {
   serverErrHandler,
   customErrorsHandler,
@@ -14,6 +17,7 @@ app.use(express.json());
 app.get("/api", getApi);
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById);
+app.get("/api/articles", getAllArticles);
 app.all("*", endpointHandler);
 
 app.use(psqlErrHandler);
--- db/models/articles.models.js
+++ db/models/articles.models.js
@@ -10,3 +10,21 @@ exports.selectArticleById = (article_id) => {
       return rows[0];
     });
 };
+exports.fetchArticles = () => {
+  const queryString = `SELECT articles.author,
+        articles.title,
+        articles.article_id,
+        articles.topic,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url, 
+        COUNT(comments.article_id) AS comment_count 
+        FROM articles
+        LEFT JOIN comments
+        ON comments.article_id = articles.article_id
+        GROUP BY articles.article_id 
+        ORDER BY articles.created_at DESC;`;
+  return db.query(queryString).then(({ rows }) => {
+    return rows;
+  });
+};
