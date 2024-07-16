4bfd3f361dec0d136aa37df769f27150640e47db
OpaogunProsper <prblvck@gmail.com>
2024-07-16 13:06:52 +0100

    gets articles by id

--- db/app.js
+++ db/app.js
@@ -2,18 +2,21 @@ const express = require("express");
 const app = express();
 const { getApi } = require("./controllers/api.controllers");
 const { getTopics } = require("./controllers/topics.controllers");
+const { getArticleById } = require("../db/controllers/articles.controllers");
 const {
   serverErrHandler,
-  invalidApiErrHandler,
   customErrorsHandler,
+  psqlErrHandler,
+  endpointHandler,
 } = require("./errors/error-handlers");
 app.use(express.json());
 
 app.get("/api", getApi);
 app.get("/api/topics", getTopics);
-app.all("*", invalidApiErrHandler);
+app.get("/api/articles/:article_id", getArticleById);
+app.all("*", endpointHandler);
 
-app.use(serverErrHandler);
-
+app.use(psqlErrHandler);
 app.use(customErrorsHandler);
+app.use(serverErrHandler);
 module.exports = app;
